585a1b38393a25213ce0d4d35bfb3316574ec219
Aidan Welch <aidan@freedwave.com>
2023-02-01 17:39:05 +0100

    Improve coverage of types testing

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -2,10 +2,10 @@ import { expectType } from 'tsd';
 import { translate, Translator, googleTranslateApi } from '..';
 
 expectType<Promise<googleTranslateApi.TranslationResponse>>(translate('abc'));
-expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translate(['a', 'b', 'c']));
-expectType<Promise<{a: googleTranslateApi.TranslationResponse}>>(translate({a: 'test'}));
+expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(translate({a: 'test', b: {text: 'b', to: 'nl'}}));
 
 const translator = new Translator();
 expectType<Promise<googleTranslateApi.TranslationResponse>>(translator.translate('abc'));
-expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translator.translate(['a', 'b', 'c']));
-expectType<Promise<{a: googleTranslateApi.TranslationResponse}>>(translator.translate({a: 'test'}));
+expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translator.translate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(translator.translate({a: 'test', b: {text: 'b', to: 'nl'}}));
