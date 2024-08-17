2eebb130b5d8d4b4b12b302507edabed43690a36
Seya <31957516+sglkc@users.noreply.github.com>
2024-08-17 12:07:29 +0700

    Fix type lint

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,14 +1,14 @@
 import { expectType } from 'tsd';
-import { translate, Translator, googleTranslateApi, speak } from '..';
+import { translate, Translator, speak, TranslationResponse } from 'google-translate-api-x';
 
-expectType<Promise<googleTranslateApi.TranslationResponse>>(translate('abc'));
-expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translate(['a', {text: 'b', to: 'nl'}, 'c']));
-expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(translate({a: 'test', b: {text: 'b', to: 'nl'}}));
+expectType<Promise<TranslationResponse>>(translate('abc').then(a => { console.log(a); return a }));
+expectType<Promise<TranslationResponse[]>>(translate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: TranslationResponse, b: TranslationResponse}>>(translate({a: 'test', b: {text: 'b', to: 'nl'}}));
 
 const translator = new Translator();
-expectType<Promise<googleTranslateApi.TranslationResponse>>(translator.translate('abc'));
-expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translator.translate(['a', {text: 'b', to: 'nl'}, 'c']));
-expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(translator.translate({a: 'test', b: {text: 'b', to: 'nl'}}));
+expectType<Promise<TranslationResponse>>(translator.translate('abc'));
+expectType<Promise<TranslationResponse[]>>(translator.translate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: TranslationResponse, b: TranslationResponse}>>(translator.translate({a: 'test', b: {text: 'b', to: 'nl'}}));
 
 expectType<Promise<string>>(speak('abc'));
 expectType<Promise<string[]>>(speak(['a', {text: 'b', to: 'nl'}, 'c']))
